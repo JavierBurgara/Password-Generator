b50d799a4042f03bb8eb976c2f5b3c5f8a8435d6
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-01 13:48:03 -0700

    assign var to certain names in script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment code here
-
-
+var charactersLength = 8;
+var array = [];
+var specialCharacterArray = ['!','@','#','$','%','^','&','*','(',')','_','+','-','=', ',','<','.','>','/','?',';',':','[',']','{','}',];
+var lowerCaseArray = ['a','z','q','w','s','x','e','d','c','r','f','v','t','g','b','y','h','n','u','j','m','i','k','o','l','p',];
+var upperCaseArray = ['A','Z','Q','W','S','X','E','D','C','R','F','V','T','G','B','Y','H','N','U','J','M','I','K','O','L','P',];
+var numberArray =['1','2','3','4','5','6','7','8','9','0',];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
